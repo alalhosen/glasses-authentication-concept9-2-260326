16950175cab74cc10379fef42a40272b428b621d
Alal Hosen <alalhosen1992620120@gmail.com>
2026-04-06 13:44:37 +0600

    register form setup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="w-[40%] mx-auto min-w[500px] border-2 border-red-500 p-2 rounded-xl text-left">
+      <form onSubmit={handleRegister}>
         <div>
-            <h1>This is login page.</h1>
+          <p>Name</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
         </div>
-    );
+        <div>
+          <p>Photo</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div>
+          <p>Email</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div>
+          <p>Password</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div>
+          <p>Confirm password</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </div>
+        <button className="btn btn-primary w-full">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
