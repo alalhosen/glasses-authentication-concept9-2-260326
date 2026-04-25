efea2d4372d0ff470614fe71199b4ef3648ab114
Alal Hosen <alalhosen1992620120@gmail.com>
2026-04-25 22:25:13 +0600

    productDetails component create

---
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <h1>Product</h1>
+        </div>
+    );
+};
+
+export default ProductDetails;<h1>Product</h1>
