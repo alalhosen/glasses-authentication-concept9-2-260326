7015d0573f25d862c40c603f95388e3af7b652bd
Alal Hosen <alalhosen1992620120@gmail.com>
2026-05-01 21:10:19 +0600

    use navigate & state

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
   if (!user) {
-    return <h1>loading</h1>;
+    return <Navigate to='/login' state={}/>
   }
 
   return <div>{children}</div>;
