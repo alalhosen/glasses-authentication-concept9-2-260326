49037c1cecd832481f44b4aae0f34c05715a2ecb
Alal Hosen <alalhosen1992620120@gmail.com>
2026-05-03 23:27:51 +0600

    use navigate & location

--- src/components/Login/SocialLogin.jsx
+++ src/components/Login/SocialLogin.jsx
@@ -1,13 +1,23 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
 const SocialLogin = () => {
-  const { googleLogin, githubLogin,twitterLogin } = useAuth();
+  const { googleLogin, githubLogin, twitterLogin } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSocialLogin = (socialProvider) => {
+    socialProvider().then((result) => {
+      console.log(result.user);
+    });
+  };
+
   return (
     <>
       <div className="divider">Continue With</div>
       <div className="flex justify-around">
         <button
-          onClick={() => googleLogin()}
+          onClick={() => handleSocialLogin(googleLogin)}
           className="btn btn-primary btn-sm btn-outline"
         >
           Google
